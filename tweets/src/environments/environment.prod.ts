4ce91ea41fa74c5286d239921e07245e7ae86e10
Tabitha-11 <tabitharavichandran0511@gmail.com>
2021-12-20 23:39:42 +0530

    app deployed

--- tweets/src/environments/environment.prod.ts
+++ tweets/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: 'https://api.twitter.com/2/tweets/search/recent',
+  key: 'zWcPvLUwD2VCtAPqFCJSLWibL',
+  key_secret: 'yHv00qjSuUDAZ1DShZTZnvIRsiH99iOWbdcnnpkIvYCqUXWHQv',
+  bearer_token:
+    'Bearer AAAAAAAAAAAAAAAAAAAAAAnmXAEAAAAAnlr3RFGe7956JOac2LiphPW3uZ0%3DhNix6xBp6ffKfE9kbFQkczawaASdmXmQiERz3JEI4pbaFe3bYp',
 };
